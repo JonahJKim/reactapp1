d9d2303938b0f51731f92a18d21cea119cc57018
Jonah Kim <jonahkim03@gmail.com>
2022-04-30 18:30:43 -0700

    Fully interactive todo list

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,13 +1,27 @@
+import { useState } from 'react';
+import Modal from './Modal';
+import Backdrop from './Backdrop';
+
 function Todo(props) {
+    const [modalIsOpen, setModalisOpen] = useState(false);
+
+
     function deleteHandler() {
-        alert('Clicked!')
+        setModalisOpen(true);
     }
+
+    function closeModalHandler() {
+        setModalisOpen(false);
+    }
+
     return (
         <div className='card'>
         <h2>{props.text}</h2>
         <div className='actions'>
           <button className="btn" onClick={deleteHandler}>Delete</button>
         </div>
+        {modalIsOpen && <Modal onCancel={closeModalHandler} onConfirm={closeModalHandler}/>}
+        {modalIsOpen && <Backdrop onClick={closeModalHandler}/>}
       </div>
     )
 }
